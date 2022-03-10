5cec51ee3a07a13ef57bd2eda467d348966d22b7
jobnovice <jobkebede47@gmail.com>
2022-03-10 22:58:16 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-int main(void)
-{
-	 char text[] = "\"Programming is like building a multilingual puzzle";
-	 puts(text);
-	 return (0);
+
+int main(void){
+
+   char text[] = "\"Programming is like building a multilingual puzzle";
+	 
+   puts(text);
+	
+   return (0);
 }
