7b1dc9d1b1aee369b6636d660f73c15f7e80b728
jobnovice <jobkebede47@gmail.com>
2022-03-11 20:22:47 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <=57 ; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j <= 102; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
