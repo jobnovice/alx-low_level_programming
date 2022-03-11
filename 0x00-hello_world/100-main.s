8ef52f191a27ff38ff85468bc6d5075c51c8e683
jobnovice <jobkebede47@gmail.com>
2022-03-11 00:50:52 +0000

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/100-main.s
@@ -0,0 +1,30 @@
+	.file	"100-main.c"
+	.intel_syntax noprefix
+	.section	.rodata
+.LC0:
+	.string	"Holberton School"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	push	ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	mov	ebp, esp
+	.cfi_def_cfa_register 5
+	and	esp, -16
+	sub	esp, 16
+	mov	DWORD PTR [esp], OFFSET FLAT:.LC0
+	call	printf
+	mov	eax, 0
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
