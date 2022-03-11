35de42b225086b8574c1a84ef98af3f98f3640fb
jobnovice <jobkebede47@gmail.com>
2022-03-11 05:29:10 +0300

    new change

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,20 @@
  */
 int main(void)
 {
-	putchar('a','n','d',' ','t','h','a','t');
-	putchar(' ','p','i','e','c','e',' ','o');
-	putchar('f',' ','a','r','t',' ','i','s',' ','u','s','e')
-	putchar('f','u','l','"',' ','-',' ','D','o','r','a',' ','K','o','r');
-	putchar('p','a','r',',',' ','2','0','1','5','-','1','0'):
+	putchar('a','n','d',' ');
+	putchar('t','h','a','t');
+	putchar(' ','p','i','e');
+	putchar('c','e',' ','o');
+	putchar('f',' ','a','r');
+	putchar('t',' ','i','s');
+	putchar(' ','u','s','e');
+	putchar('f','u','l','"');
+	putchar(' ','-',' ','D');
+	putchar('o','r','a',' ');
+	putchar('K','o','r');
+	putchar('p','a','r',',');
+	putchar(' ','2','0','1');
+	putchar('5','-','1','0');
 	putchar('-','1','9','\n');
 	return (1);
 }
