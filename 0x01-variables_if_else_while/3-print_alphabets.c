11b5d01412926bacdefe760f5e8018dc0421f7c6
jobnovice <jobkebede47@gmail.com>
2022-03-11 19:55:27 +0000

    correct ouput

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (j = 65; j <= 90; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
