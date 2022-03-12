c8043b474887f93866b671134d933796cea204d0
jobnovice <jobkebede47@gmail.com>
2022-03-12 09:50:46 +0000

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 
 /**
@@ -7,20 +8,6 @@
  */
 int main(void)
 {
-	putchar('a','n','d',' ');
-	putchar('t','h','a','t');
-	putchar(' ','p','i','e');
-	putchar('c','e',' ','o');
-	putchar('f',' ','a','r');
-	putchar('t',' ','i','s');
-	putchar(' ','u','s','e');
-	putchar('f','u','l','"');
-	putchar(' ','-',' ','D');
-	putchar('o','r','a',' ');
-	putchar('K','o','r');
-	putchar('p','a','r',',');
-	putchar(' ','2','0','1');
-	putchar('5','-','1','0');
-	putchar('-','1','9','\n');
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
