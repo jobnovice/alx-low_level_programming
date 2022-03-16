3b3a04e9bbd03907e89b5ff305889b65c819f3c9
jobnovice <jobkebede47@gmail.com>
2022-03-16 19:11:42 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * print alphabet is the function
+ * Return: void nothing to return
+ * that's all
+ */
+void print_alphabet_x10(void);
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 94; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
