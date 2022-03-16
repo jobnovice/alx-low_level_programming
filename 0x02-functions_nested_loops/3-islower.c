c607eec708f81acbc3acc12feb4448a3d2829b67
jobnovice <jobkebede47@gmail.com>
2022-03-16 19:40:38 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,13 @@
 /**
  * _islower - Entry point
  * _islower is the function
+ * c is an integer
  * Return: an int value will be returned
  * that's all
  */
 int _islower(int c)
-{	char r;
-	r = _putchar(c);
-	if (r >= 97 || r <= 122)
+{
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
