98fd0b5c27c99c4edc864348051a81bd8e7f00f8
jobnovice <jobkebede47@gmail.com>
2022-03-22 20:16:15 +0000

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - void function
+ * @n: is the fisrt pointer
+ * Return - no return type void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
