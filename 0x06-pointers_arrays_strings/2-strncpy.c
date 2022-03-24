03523e1d1f4cf9b600e7e2481dfcd262b7f8b69f
jobnovice <jobkebede47@gmail.com>
2022-03-24 21:27:47 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strcpy - main function
+ * *_strncpy - main function
  *@dest:destination string
  *@src:source string
  *@n:number of times
@@ -9,14 +9,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	for (j = 0;src[j] != '\0' && j < n; j++)
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[j] = src[j];
 		i++;
 	}
-	for ( ;j < n; j++)
+	for ( ; j < n; j++)
 	{
 		dest[j] = '\0';
 	}
