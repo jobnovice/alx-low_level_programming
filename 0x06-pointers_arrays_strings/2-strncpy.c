590daedc3141dce6d3ef1c864451ffc1870b8a58
jobnovice <jobkebede47@gmail.com>
2022-03-24 21:25:04 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (j = 0;src[j] != '\0' && j <= n; j++)
+	for (j = 0;src[j] != '\0' && j < n; j++)
 	{
 		dest[j] = src[j];
 		i++;
