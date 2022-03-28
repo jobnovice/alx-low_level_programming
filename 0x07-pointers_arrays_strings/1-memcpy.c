b226ed29e2b85b156f28fa00c36c536d23542b90
jobnovice <jobkebede47@gmail.com>
2022-03-28 19:31:46 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ **_memcpy - the main function that returns main
+ *@dest:destination char array
+ *@src: source array 
+ *@n: at most n bytes
+ *Return: char 
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
 int _putchar(char c);
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
