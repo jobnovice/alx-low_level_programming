82fc7a336793169893f8e494eb8466ff310c9930
jobnovice <jobkebede47@gmail.com>
2022-03-28 20:27:41 +0000

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,5 @@ void simple_print_buffer(char *buffer, unsigned int size);
 int _putchar(char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
