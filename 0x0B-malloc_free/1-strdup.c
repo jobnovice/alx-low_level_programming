8e122a3bb7b66a0ab1e8edb7c241a7e5077e7b78
jobnovice <jobkebede47@gmail.com>
2022-04-06 21:20:06 +0300

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ **create_array -  Create a array object
+ * @str: yeah man the size of thea array. 
+ * Return: char*  if there is no mo space
+ */
+
+char *_strdup(char *str)
+{
+    char *array;
+    int i,size;
+
+   
+	if (str == '\0')
+		return (NULL);
+    while (str[i] != '\0')
+     {
+         size++;
+         i++;
+     }
+	array = (char *) malloc(sizeof(char) * size);
+
+    for (i = 0; str[i]; i++)
+        array[i]= str[i];
+	return (array);
+}
