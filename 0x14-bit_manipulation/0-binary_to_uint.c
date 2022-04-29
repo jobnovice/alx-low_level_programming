14fbc4b74dbd3368b525e4ca8d02982c5968b8d3
jobnovice <jobkebede47@gmail.com>
2022-04-29 20:56:45 +0000

    yeah sol ong

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <math.h>
 unsigned int binary_to_uint(const char *b)
 {
     unsigned int i,n=0;
-    if (b == '\0')
+
+    if (*b == '\0')
         return 0;
-    for (i=0;b[i]='\0';i++)
+    for (i=0; b[i]!='\0'; i++)
     {
-        if(b[i] != ('0' || '1') )
+        if(b[i] != '0' || b[i] !=  '1')
             return 0;
     }
-    for (i=0;b[i]='\0';i++)
+    for (i=0;b[i]!='\0';i++)
     {
         if(b[i] == '0')
            n+= 2^i * 0;
