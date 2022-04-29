5e84703469af5e21b23a7a8e936edce8715519a6
Eyob <jobkebede47@gmail.com>
2022-04-29 11:47:50 +0300

    added some files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int i,n=0;
+    if (b == '\0')
+        return 0;
+    for (i=0;b[i]='\0';i++)
+    {
+        if(b[i] != ('0' || '1') )
+            return 0;
+    }
+    for (i=0;b[i]='\0';i++)
+    {
+        if(b[i] == '0')
+           n+= 2^i * 0;
+        else 
+            n+= 2^i * 1;
+    }
+    return n;
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef file_main
+#define file_main
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
