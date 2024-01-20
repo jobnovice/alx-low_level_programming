fa01a4d9e6a2c788bb995b82e5424089f3722bba
Eyob <jobkebede47@gmail.com>
2024-01-21 00:39:42 +0300

    psuhed one in a while

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - search algorthim to retreive an item 
+ * @array: the list of which the value to be searched in
+ * @size: the size of the array
+ * @value: the value to be searched
+ * Returns: the index of the value to be searched
+ */
+ int linear_search(int *array, size_t size, int value)
+ {
+     if (array == NULL) return -1;
+     for (int i = 0; i < size; i++){
+        if (array[i] == value){
+            return i;
+        }
+     }
+     return -1;
+ }
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+# search algorthims implemented
