472824d106b2085456324c3eb99ec659aa0f87bf
jobnovice <jobkebede47@gmail.com>
2024-02-06 03:15:58 +0300

    interpolated

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,39 +1,49 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - another search algorthim that searches a value based on finding the probable 
- * location of the list
- * 
- * @array: the list to perform the alogrthim on 
+ * interpolation_search - another search algorthim that searches a
+ * value based on finding the probable location of the list
+ * @array: the list to perform the alogrthim on
  * @size: the size of the array
  * @value: the value to search for
- * Returns - the index of the value or -1 
+ * Return: the index of the value or -1
  */
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	int i, low, high, pos1;
+	int low, high;
+	int pos;
+
 	low = 0;
 	high = (int) size - 1;
-	size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
+	pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
-	if ( ! array){
-		return -1;
+	if (!array)
+	{
+		return (-1);
 	}
 
-	for (i = 0; i <= (int) size -1; i++)
-	{	
-		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
+	while (low <= high)
+	{
+		if (pos > (int) size - 1)
+		{
+			printf("Value checked array[%d] is out of range\n", pos);
+			return (-1);
+		}
 		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
 
-		if (array[pos] == value){
-			return pos;
+		if (array[pos] == value)
+		{
+			return (pos);
 		}
-		else if (array[pos] > value){
-			high = pos -1;
+		else if (array[pos] > value)
+		{
+			high = pos - 1;
 		}
-		else {
+		else
+		{
 			low = pos + 1;
 		}
 	}
